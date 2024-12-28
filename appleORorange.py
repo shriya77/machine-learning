774da7df8138455d4370e0e3aeb58be66ea3daad
Shriya Kalyan <shriyakkx@gmail.com>
2024-12-28 07:29:46 -0600

    Update appleORorange.py

--- appleORorange.py
+++ appleORorange.py
@@ -1,6 +1,6 @@
-from sklearn import tree
-features = [[140,1], [130,1], [150,0], [170,0]]
-labels = [0, 0, 1, 1]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(feautures, labels)
-print clf.predict([[150, 0]])
+from sklearn import tree #importing decision tree 
+features = [[140,1], [130,1], [150,0], [170,0]] #first the weight and 1 for smooth 0 for bumpy
+labels = [0, 0, 1, 1] #0 for orange 1 for apple
+clf = tree.DecisionTreeClassifier() #clf is an instance of decision tree classifier
+clf = clf.fit(feautures, labels) #trains the decision tree using given features and labels
+print clf.predict([[150, 0]]) #asks decision tree to predict label of new data point
